936126ba2698439d20154100816dc19c94b9af5e
FadeZhanger <dzhan96@uwo.ca>
2018-08-16 14:44:15 -0400

    Updated index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import './app.scss';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { AppContainer as HotReloader } from 'react-hot-loader';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import {AppContainer as HotReloader} from 'react-hot-loader';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../src/reducers';
 import App from './app';
@@ -25,9 +25,18 @@ const render = (Component: React.ComponentClass) => {
 
 render(App);
 
-if ((module as any).hot) {
-  (module as any).hot.accept('./app', () => {
+interface CustomModule extends NodeModule {
+  hot: {
+    accept: (targetPath: string, callback: () => void) => void;
+  };
+}
+
+if ((module as CustomModule).hot) {
+  (module as CustomModule).hot.accept('./app', () => {
     const NextApp = require('./app').default;
     render(NextApp);
+  });
+  (module as CustomModule).hot.accept('./reducers', () => {
+    store.replaceReducer(require('./reducers').default);
   });
 }
